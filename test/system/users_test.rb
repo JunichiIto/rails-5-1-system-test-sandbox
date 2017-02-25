14999bfb6470981dfba43913f9f3a2435f160213
Junichi Ito <jit@sonicgarden.jp>
2017-02-26 06:45:57 +0900

    Refine test

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -2,22 +2,29 @@ require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
   setup do
-    @user = User.create!(name: '伊藤淳一')
+    # テストデータの作成
+    @user = User.create!(name: 'いとう')
   end
 
   test "yubinbango.js" do
+    # User編集画面を開く
     visit edit_user_path(@user)
 
-    assert has_field?('Name', with: '伊藤淳一')
+    # Nameに"いとう"が入力されていることを検証する
+    assert has_field?('Name', with: 'いとう')
 
-    fill_in 'Postal code', with: '677-0056'
-    assert has_field?('Address', with: '兵庫県西脇市板波町')
+    # 郵便番号を入力
+    fill_in 'Postal code', with: '158-0083'
+    # 住所が自動入力されたことを検証する
+    assert has_field?('Address', with: '東京都世田谷区奥沢')
 
+    # 更新実行
     click_button 'Update User'
 
+    # 正しく更新されていること（＝画面の表示が正しいこと）を検証する
     assert has_content?('User was successfully updated.')
-    assert has_content?('伊藤淳一')
-    assert has_content?('677-0056')
-    assert has_content?('兵庫県西脇市板波町')
+    assert has_content?('いとう')
+    assert has_content?('158-0083')
+    assert has_content?('東京都世田谷区奥沢')
   end
 end
