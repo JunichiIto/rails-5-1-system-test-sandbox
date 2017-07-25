b262c92b3d6df4619d664f22d06d9222abb011e1
Junichi Ito <jit@sonicgarden.jp>
2017-07-26 07:53:24 +0900

    Update chromedriver version

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  Chromedriver.set_version "2.31"
   caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => %w(--headless --disable-gpu)})
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { desired_capabilities: caps }
 end
